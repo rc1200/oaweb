76c9cfdacb9263b1c158a6fa18329d51042e6a86
rc1200 <ron.calibuso@gmail.com>
2019-11-03 01:21:38 -0500

    mod open

--- oaweb/views.py
+++ oaweb/views.py
@@ -29,10 +29,11 @@ def open(request):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     driver.get('https://www.google.com')
+    stuffreturn = driver.page_source
 
-    return render(request,'home.html',{})
+    return render(request,'home.html',{ 'stuffreturn': stuffreturn})
